6c6b087f8e75d57f38a456c865b5392de4a1e700
Yuta Hinokuma <yuta.hinokuma725@gmail.com>
2019-01-22 14:05:13 +0900

    forward prop corner pooling

--- cornernet/functions/corner_pooling.py
+++ cornernet/functions/corner_pooling.py
@@ -12,33 +12,34 @@ class CornerPoolingLeft(object):
         z = cuda.cupy.zeros((n, c, h, w), dtype=x[0].dtype)
 
         self.indexes = cuda.cupy.empty((n, c, h, w), dtype=numpy.int32)
+        print(x[0].reshape(-1, w))
 
+        print(x[0])
         cuda.elementwise(
-            'raw T in, int32 h, int32 w',
-            'raw T out, S indexes, T z',
+            'raw T in, int32 w',
+            'raw T out, raw S indexes, S z',
             '''
-               int c0    = i / (h * w);
-               int y = i / w % h;
-               int x = i % w;
-               int prev_x = x - 1;
-               if (x == 0) {
-                   out[i] = in[i];
-                   indexes = i;
-                   return;
-               }
-               int prev_index = ((c0 * h + y) * w) + prev_x;
-               if (out[prev_index] < in[i]){
-                   out[i] = in[i];
-                   indexes = i;
-               }
-               else {
-                    out[i] = out[prev_index];
-                    indexes = prev_index;
-               }
-               z = out[prev_index];
-            ''', 'corner_pool_left_fwd')(x[0].reduced_view(), h, w, y.reduced_view(), self.indexes, z)
+            int ind[] = {i, 0};
+            out[ind] = in[ind];
+            indexes[ind] = i * w;
+            for (int j = 1; j < w; j++) {
+                int ind2[] = {i, j};
+                int prev[] = {i, j-1};
+                if (out[prev] < in[ind2]) {
+                    out[ind2] = in[ind2];
+                    indexes[ind2] = i*w+j;
+                }
+                else {
+                    out[ind2] = out[prev];
+                    indexes[ind2] = indexes[prev];
+                }
+            }
+            //z = in[i];
+            ''', 'corner_pool_left_fwd')(x[0].reshape(-1, w), w, y.reshape(-1, w), self.indexes.reshape(-1, w), cuda.cupy.arange(n * c * h, dtype=numpy.int32))
+        print(x[0])
         print(self.indexes)
-        print(z)
+        #print(z)
+        #print(x[0])
         return y
 
 if __name__ == '__main__':
@@ -47,7 +48,9 @@ if __name__ == '__main__':
     import numpy as np
     
     cp = CornerPoolingLeft()
-    a = xp.arange(9)[::-1].reshape((1, 1, 3, 3)).astype(np.float32)
+    a = xp.arange(24*2)[::-1].reshape((2, 2, 4, 3)).astype(np.float32)
+    a[0, 0, 1, 1] = 100
+    print(a.reduced_view())
     print(a)
     print(cp.forward_gpu([a]))
 
